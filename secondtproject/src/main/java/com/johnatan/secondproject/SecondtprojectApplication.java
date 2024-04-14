68a0c739ca91fb02f457231d304b9b34a74f4869
sayancookie <mr_cookie01@yahoo.com>
2024-04-14 19:54:54 -0400

    'got-mixed-up-by-onedrive'

---
+++ secondtproject/src/main/java/com/johnatan/secondproject/SecondtprojectApplication.java
@@ -0,0 +1,13 @@
+package com.johnatan.secondproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondtprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondtprojectApplication.class, args);
+	}
+
+}
---
+++ secondtproject/src/test/java/com/johnatan/secondproject/SecondtprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnatan.secondproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondtprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
